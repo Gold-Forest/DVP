952c6281b63f6776541edac3f11ac7cae8e6e517
Hashenko Dmytro <hashenko.dmytro@gmail.com>
2021-08-27 16:01:44 +0300

    Pass Chapter 1, book Django 3.0, Dronov(ru)

---
+++ bboard/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+# Create your models here.
+class Bb(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Товар')
+    content = models.TextField(null=True, blank=True,
+                               verbose_name='Опис')
+    price = models.FloatField(null=True, blank=True, verbose_name='Ціна')
+    published = models.DateTimeField(auto_now_add=True, db_index=True,
+                                     verbose_name='Опубліковано')
+
+    class Meta:
+        verbose_name_plural = 'Об\'яви'
+        verbose_name = 'Об\'ява'
+        ordering = ['-published']
